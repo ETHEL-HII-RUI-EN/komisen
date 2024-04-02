4376b127848b67e1de22806cab71f2d4038a31a1
ETHEL-HII-RUI-EN <163738486+ETHEL-HII-RUI-EN@users.noreply.github.com>
2024-04-02 09:00:31 +0800

    Update and rename kira_komisen.py 2

--- kira_komisen.py
+++ kira_komisen.py
@@ -1,10 +1,16 @@
+#Atur cara mengira komisen
+
+#Isytihar pemboleh ubah
 jumlah=0
 jualan=0
 komisen=0
 ulang="Y"
 while ulang=="Y":
+   
+    #Input
     jualan=float(input("Masukkan jumlah jualan:RM"))
-    if jualan>80:
+   #Proses
+if jualan>80:
         kadar_komisen=0.055
     elif jualan>70:
         kadar_komisen=0.05
@@ -15,8 +21,11 @@ while ulang=="Y":
     else:
         kadar_komisen=0.02
         komisen=jualan*kadar_komisen
+        #Output
         print("Komisen anda ialah RM",round(komisen,2))
         jumlah=jumlah+komisen
-        ulang=input("Masukkan Y untuk teruskan pengiraan atau N untuk hentikan pengira")
+        #Input
+       ulang=input("Masukkan Y untuk teruskan pengiraan atau N untuk hentikan pengira")
+    #Output
 print("\n\tJulah komisen ialah RM", round(jumlah.2))
 print("\n\t...Anda telah selesai membuat pengiraan...")
